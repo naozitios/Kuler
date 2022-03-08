a914dced86c365290a5b2d404200270266e42c5e
naozitios <75418081+naozitios@users.noreply.github.com>
2022-03-08 21:50:04 +0800

    Trial push

--- src/components/SigninButton.vue
+++ src/components/SigninButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <router-link to="/"><button class="button signin">Sign-in/Sign-up</button></router-link>
+    <router-link to="/"><button class="button signin">Sign-ins/Sign-up</button></router-link>
   
 </template>
 
