b9617ba2d7cf3c85ceb9d3d186212ba0a8a51855
magiciansz <80561550+magiciansz@users.noreply.github.com>
2022-03-10 00:02:47 +0800

    Add style

--- src/components/ProductScroll.vue
+++ src/components/ProductScroll.vue
@@ -1,15 +1,15 @@
 <template>
   
   <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel" >
-  <div class="carousel-indicators" style="bottom: 250px;left:250px; width: 250px; height:200px; margin: 20px; position:absolute;">
+  <div class="carousel-indicators">
   <a href="#" data-bs-target="#carouselExampleControls" data-bs-slide-to="0" class="active" ><img src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel1.jpeg"
-  style="width:50px;height:50px;position:relative; top:150px; bottom:0; left:100px; right:0; margin:auto"></a>
+  class ="indicator1"></a>
   <a href="#" data-bs-target="#carouselExampleControls" data-bs-slide-to="1"><img src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel2.jpeg"
-  style="width:50px;height:50px;position:relative; top:225px; bottom:0; left:63px; right:0; margin:auto"></a>
+  class ="indicator2"></a>
   <a href="#" data-bs-target="#carouselExampleControls" data-bs-slide-to="2"><img src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel3.jpeg"
-  style="width:50px;height:50px;position:relative; top:300px; bottom:0; left:26px; right:0; margin:auto"></a>
+  class ="indicator3"></a>
   <a href="#" data-bs-target="#carouselExampleControls" data-bs-slide-to="3"><img src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel4.jpeg"
-  style="width:50px;height:50px;position:relative; top:375px; bottom:0; left:-11px; right:0; margin:auto"></a>
+  class ="indicator4"></a>
   </div>
   
   
@@ -74,6 +74,59 @@ export default {
 
 .carousel-indicators li{display:none;}
 
+.carousel-indicators {
+  bottom: 230px;
+  left:250px; 
+  width: 250px; 
+  height:200px; 
+  margin: 20px;
+  position:absolute;
+}
+
+.indicator1 {
+  width:50px;
+  height:50px;
+  position:relative; 
+  top:150px; 
+  bottom:0; 
+  left:100px; 
+  right:0; 
+  margin:auto;
+}
+
+.indicator2 {
+  width:50px;
+  height:50px;
+  position:relative; 
+  top:225px; 
+  bottom:0; 
+  left:63px; 
+  right:0; 
+  margin:auto;
+}
+
+.indicator3 {
+  width:50px;
+  height:50px;
+  position:relative; 
+  top:300px; 
+  bottom:0; 
+  left:26px; 
+  right:0; 
+  margin:auto;
+}
+
+.indicator4 {
+  width:50px;
+  height:50px;
+  position:relative; 
+  top:375px; 
+  bottom:0; 
+  left:-11px; 
+  right:0; 
+  margin:auto;
+}
+
 h3 {
   margin: 40px 0 0;
 }
@@ -81,10 +134,7 @@ ul {
   list-style-type: none;
   padding: 0;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+
 a {
   color: #42b983;
 }
