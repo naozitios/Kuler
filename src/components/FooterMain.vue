90f18a8b458f4df9772c462e7350769cffc27f41
Seah Jia Jun <orangepufflejj@gmail.com>
2022-03-09 22:38:56 +0800

    Add social icons

--- src/components/FooterMain.vue
+++ src/components/FooterMain.vue
@@ -1,8 +1,22 @@
 <template>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
     <div id="footerContainer">
         <div id="logo1">
             <KulerLogoWhite/>
             <h6 id ="copyright-line"> &copy; 2022</h6>
+            <div id="bio">
+                <h6 id="bio-text"> Kuler aims to provide digitalization for the sales of artistic content, on a reliable and secure platform. </h6>
+            </div>
+            <div id="socials">
+                <p>
+                <a href="https://www.facebook.com/profile.php?id=100078725480774" class="fa fa-facebook"></a>
+                &nbsp;&nbsp;
+                <a href="https://twitter.com/kulerbt3103" class="fa fa-twitter"></a>
+                &nbsp;&nbsp;
+                <a href="https://www.instagram.com/instagram/?hl=en" class="fa fa-instagram"></a>
+                </p>
+            </div>
         </div>
         
         <div id = "faqLinks">
@@ -37,6 +51,12 @@ export default {
 </script>
 
 <style scoped>
+.fa.fa-facebook, .fa.fa-twitter, .fa.fa-instagram {
+    color: #FFFFFF;
+    text-decoration: none;
+    font-size: 200%;
+
+}
 #logo1 {
     padding: 1em;
     float: left;
@@ -49,10 +69,19 @@ export default {
     background-color: #F37381;
 }
 
-#copyright-line {
+#bio {
+    width: 30%;
+}
+#copyright-line{
     color: #FFFFFF;
     display: inline;
 
+}
+
+#bio-text {
+    color: #FFFFFF;
+    display: block;
+    text-align: left;
 }
 
 </style>
