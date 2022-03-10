1d7ddcc58ebe5b589daf81453c34921728847cec
Seah Jia Jun <orangepufflejj@gmail.com>
2022-03-10 22:18:46 +0800

    Edit popular and top picks section

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
 import naive from "naive-ui";
+import vmodal from 'vue-js-modal'
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
 
-createApp(App).use(router).use(naive).mount('#app')
+createApp(App).use(router).use(naive).use(vmodal).mount('#app')
