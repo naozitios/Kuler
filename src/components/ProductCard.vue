e06276309caab8e936fb286dcd0133b3d57f64b9
Seah Jia Jun <orangepufflejj@gmail.com>
2022-03-10 18:29:59 +0800

    Add eight sample product pictures, initialize product card component

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Product Title</h5>
+    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tincidunt tellus at mollis dictum. Phasellus sit amet fringilla ex. </p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
