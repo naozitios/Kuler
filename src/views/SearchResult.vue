47c4a6b30626c85c51335f5f6a11c8b8f66a1b01
naozitios <75418081+naozitios@users.noreply.github.com>
2022-03-10 14:16:29 +0800

    Install Bootstrap

--- src/views/SearchResult.vue
+++ src/views/SearchResult.vue
@@ -1,74 +1,63 @@
 <template>
   <div class="banner" id="banner-home">
-    <n-image
+    <ProductScroll/>
+    <img
       src="https://images.unsplash.com/photo-1533481051286-d6396b74c877?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1691&q=80"
-      object-fit="cover"
+      class="img-fluid"
+      alt="..."
     />
   </div>
   <!--buggy sortbybutton that has to be fixed-->
   <div>
-    <SortByButton />
+    <h2>
+      Hi my name is NOah
+    </h2>
   </div>
-
-  <div class="work-page">
-    <n-grid x-gap="12" :cols="4">
-      <n-gi>
-        <n-card title="Card">
-          <template #cover>
-            <img
-              src="https://images.unsplash.com/flagged/photo-1572392640988-ba48d1a74457?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80"
-            />
-          </template>
-          Product Details
-        </n-card>
-      </n-gi>
-      <n-gi>
-        <n-card title="Card">
-          <template #cover>
-            <img
-              src="https://images.unsplash.com/photo-1605721911519-3dfeb3be25e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-            />
-          </template>
-          Product Details
-        </n-card>
-      </n-gi>
-      <n-gi>
-        <n-card title="Card">
-          <template #cover>
-            <img
-              src="https://images.unsplash.com/photo-1536924940846-227afb31e2a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1766&q=80"
-            />
-          </template>
-          Product Details
-        </n-card>
-      </n-gi>
-      <n-gi>
-        <n-card title="Card">
-          <template #cover>
-            <img
-              src="https://images.unsplash.com/photo-1561214115-f2f134cc4912?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=818&q=80"
-            />
-          </template>
-          Product Details
-        </n-card>
-      </n-gi>
-    </n-grid>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <div class="card" style="width: 18rem">
+          <img src="https://images.unsplash.com/photo-1533481051286-d6396b74c877?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1691&q=80" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card" style="width: 18rem">
+          <img src="https://images.unsplash.com/photo-1533481051286-d6396b74c877?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1691&q=80" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card" style="width: 18rem">
+          <img src="https://images.unsplash.com/photo-1533481051286-d6396b74c877?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1691&q=80" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-
 <style>
-.n-card {
-  width: 18vmax;
-  height: 200px;
-}
-.light-green {
-  height: 108px;
-  background-color: rgba(0, 128, 0, 0.12);
-}
-.green {
-  height: 108px;
-  background-color: rgba(0, 128, 0, 0.24);
-}
 .work-page {
   width: 80vmax;
   margin-left: auto;
@@ -82,14 +71,18 @@
 </style>
 
 <script>
-import { defineComponent } from "vue";
-import SortByButton from "../components/SortByButton.vue";
+import ProductScroll from '@/components/ProductScroll.vue'
 
-export default defineComponent({
-  setup() {},
-  name: "App",
-  components: {
-    SortByButton,
+export default {
+  name: 'App',
+  components:{
+    ProductScroll,
   },
-});
+  data(){
+    
+  },
+  methods:{
+   
+  }
+}
 </script>
