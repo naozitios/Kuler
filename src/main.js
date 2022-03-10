f7337d19874b8b46035756d4514490c5c56eeeac
Seah Jia Jun <orangepufflejj@gmail.com>
2022-03-10 22:19:31 +0800

    Remove vModal dependency

--- src/main.js
+++ src/main.js
@@ -2,9 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
 import naive from "naive-ui";
-import vmodal from 'vue-js-modal'
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
 
-createApp(App).use(router).use(naive).use(vmodal).mount('#app')
+createApp(App).use(router).use(naive).mount('#app')
