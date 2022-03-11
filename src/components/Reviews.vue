0b74346fa415392bb7b7208c14289eab6ac79eb8
andrewwongss <e0543700@u.nus.edu>
2022-03-11 15:39:55 +0800

    add reviews

---
+++ src/components/Reviews.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id = "section-main">
+        <h5 id = "section-title">Reviews</h5>
+        <StarRating/>
+    </div>
+</template>
+
+<script>
+import StarRating from '@/components/StarRating.vue'
+
+export default {
+    name: 'Reviews',
+    components:{
+    StarRating
+  },
+  data(){
+    
+  },
+  methods:{
+   
+  }
+}
+</script>
+
+<style scoped>
+#section-main {
+    padding: 5em 2em 0em 2em;
+    background-color: #F7F0DD;
+}
+#section-main #section-title{
+    font-weight: 900;
+    text-align: left;
+}
+
+
+</style>
