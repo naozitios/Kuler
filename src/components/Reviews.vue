3c4cc356fcbfd2d82051c43ce8cf00db46e5ff9a
andrewwongss <e0543700@u.nus.edu>
2022-03-12 13:05:21 +0800

    update reviews

--- src/components/Reviews.vue
+++ src/components/Reviews.vue
@@ -1,6 +1,19 @@
 <template>
     <div id = "section-main">
         <h5 id = "section-title">Reviews&nbsp;<StarRating/></h5>
+
+        <!-- reviewers -->
+        <div id= "reviewer">
+            Andrew Wong &nbsp;&nbsp;13/2/22&nbsp;&nbsp;<StarRating/><!--change name to DB-->
+        </div>
+        <div id= "product-text"> 
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla lobortis aliquam cursus.
+          Aenean tortor odio, maximus at nisi in, 
+          condimentum convallis ex. Nunc eu justo efficitur lectus iaculis maximus id sed lorem. 
+      </div>
+      <div id= "reviewer">
+            Noah Teo &nbsp;&nbsp;25/2/22&nbsp;&nbsp;<StarRating/><!--change name to DB-->
+        </div>
         <div id= "product-text"> 
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla lobortis aliquam cursus.
           Aenean tortor odio, maximus at nisi in, 
@@ -39,6 +52,10 @@ export default {
     font-weight: 900;
     text-align: left;
     display: flex;
+}
+#reviewer{
+    padding-top:2em;
+    display:flex;
 }
 #product-text{
     display:flex;
