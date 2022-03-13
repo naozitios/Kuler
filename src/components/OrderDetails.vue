58de3fda9d79797ba53c392ed9a186ea5cad4435
naozitios <75418081+naozitios@users.noreply.github.com>
2022-03-13 13:04:10 +0800

    make orderdetails component

---
+++ src/components/OrderDetails.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class='row'>
+        <h1> Order Details </h3>
+        <h3> Information > Shipping > Payment </h3>
+    </div>
+        <div class='row'>
+            <h1> Name </h1>
+            <input type="text" class="Name" placeholder="Enter Name">
+    </div>
+            <div class='row'>
+            <h1> Email Address </h1>
+            <input type="text" class="EmailAddress" placeholder="Enter Email">
+    </div>
+    <ContinueToShippingButton />
+</template>
+
+<script>
+import ContinueToShippingButton from '@/components/ContinueToShippingButton.vue'
+
+export default {
+    name: 'OrderDetails',
+  components:{
+    ContinueToShippingButton,
+  },
+  data(){
+    
+  },
+  methods:{
+   
+  }
+
+
+}
+</script>
+
+<style scoped>
+
+</style>
