e3320a998f53b593b43c58a677b99ba17f83ec1f
naozitios <75418081+naozitios@users.noreply.github.com>
2022-03-13 13:04:56 +0800

    make payment details

---
+++ src/components/PaymentDetails.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class='row'>
+        <h1> Order Details </h3>
+        <h3> Information > Shipping > Payment </h3>
+    </div>
+        <div class='row'>
+            <h1> Card Details </h1>
+            <input type="text" class="CardNumber" placeholder="Card Number">
+        </div>
+            <div class='row'>
+            <h1> Name on card </h1>
+            <input type="text" class="CardName" placeholder="Name">
+          </div>
+
+            <div class='row'>
+            <h1> CVV </h1>
+            <input type="text" class="CVV" placeholder="CVV">
+    </div>
+            <div class='row'>
+            <h1> Email Address </h1>
+            <input type="text" class="EmailAddress" placeholder="Enter Email">
+    </div>
+    <PayButton />
+</template>
+
+<script>
+import PayButton from '@/components/PayButton.vue'
+
+export default {
+    name: 'OrderDetails',
+  components:{
+    PayButton,
+  },
+  data(){
+    
+  },
+  methods:{
+   
+  }
+
+
+}
+</script>
+
+<style scoped>
+
+</style>
