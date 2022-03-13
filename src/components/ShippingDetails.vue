5c1cb2813db321535df955c603b9e991ee9d10c0
naozitios <75418081+naozitios@users.noreply.github.com>
2022-03-13 13:06:12 +0800

    make shipping details, route around cart payment feature

---
+++ src/components/ShippingDetails.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class='row'>
+        <h1> Order Details </h3>
+        <h3> Information > Shipping > Payment </h3>
+    </div>
+        <div class='row'>
+            <h1> Name </h1>
+            <input type="text" class="Name" placeholder="Enter Name">
+    </div>
+            <div class='row'>
+            <h1> Email Address </h1>
+            <input type="text" class="EmailAddress" placeholder="Enter Email">
+    </div>
+    <ContinueToPaymentButton />
+</template>
+
+<script>
+import ContinueToPaymentButton from '@/components/ContinueToPaymentButton.vue'
+
+export default {
+    name: 'OrderDetails',
+  components:{
+    ContinueToPaymentButton,
+  },
+  data(){
+    
+  },
+  methods:{
+   
+  }
+
+
+}
+</script>
+
+<style scoped>
+
+</style>
