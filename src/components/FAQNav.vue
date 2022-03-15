f61eaea06e92493ec3ba5720049acff7197ef2e3
Seah Jia Jun <orangepufflejj@gmail.com>
2022-03-15 10:55:23 +0800

    Edit labels of FAQ nav

--- src/components/FAQNav.vue
+++ src/components/FAQNav.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+<div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
   <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a>
   <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</a>
   <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</a>
   <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</a>
 </div>
 <div class="tab-content" id="v-pills-tabContent">
-  <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">Lorem</div>
-  <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">Ipsum</div>
-  <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">Doret</div>
-  <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">Sit amet</div>
+  <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">This is home</div>
+  <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">This is profile</div>
+  <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">This is messages</div>
+  <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">This is Settings</div>
 </div>
 </template>
 
@@ -19,5 +19,23 @@ export default {
 }
 </script>
 
+
 <style scoped>
+.nav.flex-column.nav-pills{
+    width:20%;
+}
+.nav.flex-column.nav-pills a{
+    border-radius: 0px;
+    color: #FFFFFF;
+    background-color: #F37381;
+}
+.nav.flex-column.nav-pills a:hover{
+    border-radius: 0px;
+    color: #FFFFFF;
+    background-color: #dd6b79;
+}
+.nav.flex-column.nav-pills a:active, .nav.flex-column.nav-pills a:focus {
+    color: #F37381;
+    background-color: #F7F0DD;
+}
 </style>
