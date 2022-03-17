d7bb5abeb997df3915c62d2953cbcf77d71ff440
Seah Jia Jun <orangepufflejj@gmail.com>
2022-03-18 00:48:19 +0800

    Initialize change password section

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,16 +1,22 @@
 <template>
   
 <PublicProfile/>
+<PrivateInformation/>
+<ChangePassword/>
 </template>
 
 <script>
 
-import PublicProfile from '@/components/PublicProfile.vue'
+import PublicProfile from '@/components/PublicProfile.vue';
+import PrivateInformation from '@/components/PrivateInformation.vue';
+import ChangePassword from '@/components/ChangePassword.vue';
 
 export default {
   name: 'App',
   components:{
-    PublicProfile
+    PublicProfile,
+    PrivateInformation,
+    ChangePassword
   },
   data(){
     
