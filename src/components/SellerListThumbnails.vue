e3d4a670621a2a3d05f5ec56edb781c2ca64f933
magiciansz <80561550+magiciansz@users.noreply.github.com>
2022-03-21 01:24:07 +0800

    finish sell page

--- src/components/SellerListThumbnails.vue
+++ src/components/SellerListThumbnails.vue
@@ -14,6 +14,7 @@
   <img src="https://imageio.forbes.com/specials-images/imageserve/5ed6636cdd5d320006caf841/The-Blackout-Tuesday-movement-is-causing-Instagram-feeds-to-turn-black-/960x0.jpg" class="img-thumbnail" alt="...">
         </div>
   </div>
+  <div class = "second-container">
   <div class ="upload">
         <div class = "cover">
 
@@ -46,10 +47,89 @@
               </div>
               </div>
   </div>
+        <div class="description">
+    <label for="titleOfProduct">Product Description </label>
+    <input type="email" class="form-control" id="productDescription" aria-describedby="emailHelp" placeholder="">
+  </div>
+  </div>
   <div class ="productInfo">
-        <div class="form-group">
+        <div class="title">
     <label for="titleOfProduct">Title </label>
-    <input type="email" class="form-control" id="titleOfProduct" aria-describedby="emailHelp" placeholder="Enter email">
+    <input type="email" class="form-control" id="titleOfProduct" aria-describedby="emailHelp" placeholder="">
+  </div>
+  <div class="price">
+    <label for="productPrice">Price (SGD) </label>
+    <input type="email" class="form-control" id="productPrice" aria-describedby="emailHelp" placeholder="">
+  </div>
+  <div class = "licenseOptions">
+        License   
+        <a href = "https://www.artismycareer.com/management/how-to-license-your-art-a-beginners-guide-to-artwork-licensing/"> Learn More </a>
+        <br>
+        <select class="custom-select">
+        <option selected>Attribution CC BY</option>
+        <option value="1">Attribution ShareALike CC BY-SA</option>
+        <option value="2">Attribution-NoDerivs CC BY-ND</option>
+        <option value="3">Attribution-NonCommercial CC BY-NC</option>
+        <option value="4">Attribution-NonCommercial-ShareAlike CC BY-NC-SA CC BY</option>
+        <option value="5">Attribution-NonCommercial-NoDerivs CC BY-NC-ND</option>
+
+</select>
+
+  </div>
+  <div class = "categoryOptions">
+        Category   
+        <br>
+        <select class="custom-select2">
+        <option selected>Choose Category</option>
+        <option value="1">Icons</option>
+        <option value="2">Photography</option>
+        <option value="3">Collectibles</option>
+        <option value="4">Audio</option>
+        <option value="5">Videos</option>
+        <option value="6">Vintage</option>
+</select>
+
+  </div>
+  <div class = "formatOptions"> 
+  Format
+  <br>
+  <div class="form-check">
+  <input class="form-check-input" type="radio" name="exampleRadios" id="digital" value="option1" checked>
+  <label class="form-check-label" for="digital">
+    Digital
+  </label>
+</div>
+<div class="form-check">
+  <input class="form-check-input" type="radio" name="exampleRadios" id="physical" value="option2">
+  <label class="form-check-label" for="physical">
+    Physical
+  </label>
+</div>
+  </div>
+  <div class = "shippingOptions"> 
+  Shipping Options
+  <br>
+  <div class="form-check">
+  <input class="form-check-input" type="radio" name="exampleRadios" id="collectionPoint" value="option1" checked>
+  <label class="form-check-label" for="collectionPoint">
+    Collection Point
+  </label>
+</div>
+<div class="form-check">
+  <input class="form-check-input" type="radio" name="exampleRadios" id="standardEconomy" value="option2">
+  <label class="form-check-label" for="standardEconomy">
+    Standard Economy
+  </label>
+</div>
+<div class="form-check">
+  <input class="form-check-input" type="radio" name="exampleRadios" id="standardExpress" value="option2">
+  <label class="form-check-label" for="standardExpress">
+    Standard Express
+  </label>
+</div>
+  </div>
+  <div class = "listButton">
+        <button class="btn btn-primary" id = "listButton" type="button"><h3>List Item</h3></button>
   </div>
   </div>
     </div>
@@ -86,16 +166,23 @@ export default {
       border-radius: 20px 20px 20px 20px;
 }
 
+.second-container {
+      flex-direction: column;
+      width: 60%;
+}
+
+
 /* main css for beige portion */
 .upload {
+      display: flex;
       background-color: beige;
       padding-left: 5%;
       margin-left: 2%;
-      padding-top: 5%;
-      padding-bottom: 5%;
-      display: flex;
+      padding-top: 10%;
+      padding-bottom: 10%;   
       padding-right: 5%;
       border-radius: 30px 30px 30px 30px; 
+      
 }
 /* blanket for flexbox, column */
 .cover {
@@ -105,7 +192,7 @@ export default {
 }
 
 .uploadpic img {
-      max-width: 50%;
+      max-width: 46%;
       padding-top: 1%;
 }
 .uploadbar {
@@ -163,15 +250,79 @@ export default {
   background-color: #F37381;
 }
 
+/* product Description */
+
+.description {
+      text-align: left;
+      font-weight: bold;
+      margin-top: 2%;
+      margin-left: 2%;
+}
+
+#productDescription {
+      background-color: lightgray;
+      height: 100px;
+      border-radius: 20px 20px 20px 20px;
+}
+
 /* right part of page: all the forms */
 .productInfo {
       text-align: left;
       margin-left: 2%;
-      
+      font-weight: bold;
+      width: 30%;
+      flex-direction: column;
 }
 
-#titleOfProduct {
+#titleOfProduct, #productPrice {
       border-radius: 10px;
       background-color: lightgray;
+      margin-top: 2%;
 }
+.price {
+      margin-top: 5%;
+}
+
+.licenseOptions, .categoryOptions, .formatOptions, .shippingOptions {
+      margin-top: 5%;
+}
+
+.custom-select {
+      border-radius: 10px;
+      background-color: lightgray;
+      width: 100%;
+      height: 35px;
+      margin-top: 2%;
+}
+
+.custom-select2 {
+      border-radius: 10px;
+      background-color: lightgray;
+      width: 100%;
+      height: 35px;
+      margin-top: 2%;
+}
+
+/* list button has id listButton for reference */
+.listButton {
+      margin-top: 2%;
+}
+
+.btn.btn-primary {
+      width: 100%;
+  position: relative;
+  display: inline-block;
+  margin: 1em;
+  padding: 1em 2em;
+  border: 0;
+  border-radius: 30px;
+  text-align: center;
+  white-space: nowrap;
+  cursor: pointer;
+  font-size: 14px;
+  color: white;
+  font-weight: bold;
+  background-color: #F37381;
+}
+
 </style>
