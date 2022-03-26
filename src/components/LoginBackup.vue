59ce1002a69d57ebed5f167a27beb9273e15ef56
Seah Jia Jun <orangepufflejj@gmail.com>
2022-03-26 22:21:42 +0800

    Create sign in page

---
+++ src/components/LoginBackup.vue
@@ -0,0 +1,65 @@
+<template>
+    <div style="text-align'center;">
+        
+        <div id = "firebaseui-auth-container"></div>
+        <div id = "pagecontent">
+            Login <br>
+        </div>
+        <h5>Kuler 2022</h5>
+    </div>
+</template>
+
+<script>
+import firebase from '@/uifire.js'
+import 'firebase/compat/auth';
+import * as firebaseui from 'firebaseui'
+import 'firebaseui/dist/firebaseui.css'
+
+export default {
+    name:"Login",
+
+    mounted() {
+        var ui = firebaseui.auth.AuthUI.getInstance();
+        if (!ui){
+            ui = new firebaseui.auth.AuthUI(firebase.auth());
+        }
+
+        var uiConfig = {
+            signInSuccessUrl: '/home',
+            signInOptions: [
+                firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+                firebase.auth.EmailAuthProvider.PROVIDER_ID,
+            ]
+        };
+        ui.start('#firebaseui-auth-container', uiConfig)
+    }
+}
+</script>
+
+<style scoped>
+#firebaseui-auth-container{
+    margin-top:50px;
+    margin-bottom: 50px;
+}
+#pagecontent{
+    height: 100px;
+    font-size: larger;
+    font-weight: bolder;
+    text-align: center;
+}
+#mainHead{
+    text-align: center;
+    text-shadow: 2px 2px grey;
+}
+#bg{
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 60%;
+}
+h5{
+    text-align: center;
+    background-color: rgb(194,202,188);
+}
+
+</style>
---
+++ src/uifire.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/compat/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCwYTwSNiWlT-e3miprueZvB47p-reBm80",
+  authDomain: "bt3103kuler-9b113.firebaseapp.com",
+  projectId: "bt3103kuler-9b113",
+  storageBucket: "bt3103kuler-9b113.appspot.com",
+  messagingSenderId: "998867990452",
+  appId: "1:998867990452:web:f60e3b86f5bbfe5e8ce4e1",
+  measurementId: "G-BKBKY1K9R3"
+};
+
+firebase.initializeApp(firebaseConfig);
+export default firebase;
