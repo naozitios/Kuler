2efb8eab92941f7df2792b0a323b30eed057c5d1
magiciansz <80561550+magiciansz@users.noreply.github.com>
2022-03-28 02:01:05 +0800

    updated cart functionality

--- src/components/xh_components/AddToCartButton.vue
+++ src/components/xh_components/AddToCartButton.vue
@@ -1,11 +1,10 @@
 <template>
-  <button class="btn btn-primary" type="button" @click="checkEntry"><h3>Add to Cart</h3></button>
+  <button class="btn btn-primary" type="button" @click="addToCart"><h3>Add to Cart</h3></button>
 </template>
 
 <script>
 import {getAuth, onAuthStateChanged} from "firebase/auth";
-/*
-import firebaseApp from '../../firebase.js';
+/*import firebaseApp from '../../firebase.js';
 import {getFirestore} from "firebase/firestore";
 import { getDoc, doc, updateDoc, arrayUnion, arrayRemove} from "firebase/firestore";
 const db = getFirestore(firebaseApp);*/
@@ -18,6 +17,10 @@ export default {
         }
     },
 
+    props: {
+        quantity: Number
+    },
+
     mounted() {
         const auth = getAuth()
         onAuthStateChanged(auth, (user) => {
@@ -28,9 +31,25 @@ export default {
     },
 
     methods: {
-        checkEntry() {
+        async addToCart() {
             if (this.user) {
-                // add to cart
+                /*
+                const ref = doc(db, "usershoppingcarts", this.user.uid)
+                // two cases: 
+                // case 1: if not inside, we set the product and its quantity accordingly to back of array
+                // case 2: if inside, find index of array, update the quantity.
+                
+                const document = await getDoc(ref)
+                const documentData = document.data()
+                var products = documentData.products
+                // case 2
+                if (products.includes(this.productNumber)) {
+                    var index = products.indexOf(this.productNumber)
+                    var quantity = documentData.
+                    await updateDoc((ref) => {
+                        
+                    })
+                }*/
                 alert("Added item to cart.") // after that, force a refresh as well
 
             } else {
