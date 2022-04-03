a5b39a65a1a60a3b526742bda746dba9cec2c643
andrewwongss <e0543700@u.nus.edu>
2022-04-04 06:10:29 +0800

    setup reviewformpage

--- src/components/Reviews.vue
+++ src/components/Reviews.vue
@@ -7,6 +7,10 @@
             <div class = "rating" style = "position:relative; left:2px; bottom:3px;">
             <StarRatingContinuous :rating= "averageRating"/>
             </div>
+            <div id = "leaveReview">
+                <ul>
+                <li><router-link to="/ReviewForm">Leave a Review</router-link></li>
+                </ul>
             </h5>
         <!--Page 1-->
         
@@ -236,5 +240,76 @@ export default {
     margin-left:2%;
     background-color: #F7F0DD;
 }
+a {
+    -moz-transition: color 0.2s ease, border-bottom 0.2s ease;
+    -webkit-transition: color 0.2s ease, border-bottom 0.2s ease;
+    -ms-transition: color 0.2s ease, border-bottom 0.2s ease;
+    transition: color 0.2s ease, border-bottom 0.2s ease;
+    text-decoration: none;
+    /* border-bottom: dotted 1px; */
+    color: inherit;
+}
+
+a:hover {
+    border-bottom-color: transparent;
+}
+#leaveReview {
+    -moz-transition: background-color 0.2s ease, border-top-left-radius 0.2s ease, border-top-right-radius 0.2s ease, padding 0.2s ease;
+    -webkit-transition: background-color 0.2s ease, border-top-left-radius 0.2s ease, border-top-right-radius 0.2s ease, padding 0.2s ease;
+    -ms-transition: background-color 0.2s ease, border-top-left-radius 0.2s ease, border-top-right-radius 0.2s ease, padding 0.2s ease;
+    transition: background-color 0.2s ease, border-top-left-radius 0.2s ease, border-top-right-radius 0.2s ease, padding 0.2s ease;
+    background-color: #ffffff;
+    color: #636363;
+    position: relative;
+    width: 100%;
+    max-width: 100%;
+    padding: 0em 0em 1em 0em;
+    /* background-color: #f7f7f7; */
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
+    cursor: default;
+    text-align: center;
+}
+
+#leaveReview ul {
+        margin: 0;
+        padding: 0;
+        list-style: none;
+    }
+
+        #nav ul li {
+            -moz-transition: margin 0.2s ease;
+            -webkit-transition: margin 0.2s ease;
+            -ms-transition: margin 0.2s ease;
+            transition: margin 0.2s ease;
+            display: inline-block;
+            margin: 0 0.35em;
+            padding: 0;
+            vertical-align: middle;
+        }
+
+            #nav ul li a {
+                -moz-transition: font-size 0.2s ease;
+                -webkit-transition: font-size 0.2s ease;
+                -ms-transition: font-size 0.2s ease;
+                transition: font-size 0.2s ease;
+                display: inline-block;
+                height: 2.25em;
+                line-height: 2.25em;
+                padding: 0 1.25em;
+                border: 0;
+                border-radius: 8px;
+                box-shadow: inset 0 0 0 1px transparent;
+            }
+
+                #nav ul li a:hover {
+                    background-color: rgba(222, 222, 222, 0.2);
+                    color: #F37381;
+                }
+
+                #nav ul li a.router-link-exact-active {
+                    background-color: rgba(222, 222, 222, 0.25);
+                    box-shadow: none;
+                }
 
 </style>
---
+++ src/views/ReviewFormPage.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class = "big-container">
+        <ReviewForm :productID = "productID"/>
+    </div>
+</template>
+
+<script>
+import ReviewForm from '@/components/ReviewForm.vue'
+
+export default {
+   name: 'ReviewFormPage',
+   components: {
+       ReviewForm
+   },
+   data() {
+       return {
+           productID: String
+       }
+   }
+}
+</script>
