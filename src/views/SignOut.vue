e66c3bda69df22ce6252163ba3dc08eb472054d9
Seah Jia Jun <orangepufflejj@gmail.com>
2022-04-07 22:46:27 +0800

    Edit css for signout button, polish signout page

--- src/views/SignOut.vue
+++ src/views/SignOut.vue
@@ -1,9 +1,14 @@
 <template>
-  <button id= "btn" @click = "signOut"> Logout </button>
+<div style="text-align:center;" v-if="user">
+      <h3>We're sad to see you go... do visit us again!</h3>
+      <br>
+  </div>
+  <button id= "btn" @click = "signOut" class="btn btn-primary"> Logout </button>
 </template>
 
 <script>
 import { getAuth, onAuthStateChanged, signOut } from "firebase/auth"
+
 export default {
     name: "LogOut",
 
@@ -33,6 +38,24 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.btn-primary {
+text-align: right;
+background-color: #F37381;
+border: None;
+margin: 1em;
+border-radius: 20px;
+padding: 0.5em 2em 0.5em 2em;
+/* float: right; */
+/* margin-right: 10% */
+}
+
+.btn.btn-primary:hover {
+    background-color: #dd6b79;
+}
+
+.btn.btn-primary:active, .btn-primary:focus, .btn-primary:visited {
+    background-color: #F37381;
+}
 
 </style>
